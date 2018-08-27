f02f963890ed5aa3321238d2bc82b500539292d5
Peter Wikström <peter@wikstrom.in>
2018-08-27 21:08:32 +0200

    added material as a feature

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,11 @@
-export * from './md-button/md-button';
-export * from './md-toolbar/md-toolbar';
-export * from './md-switch/md-switch';
+import {FrameworkConfiguration} from 'aurelia-framework';
+import {PLATFORM} from 'aurelia-pal';
+
+export function configure(config: FrameworkConfiguration) {
+  config.globalResources([
+    PLATFORM.moduleName('./md-button/md-button'),
+    PLATFORM.moduleName('./md-switch/md-switch'),
+    PLATFORM.moduleName('./md-toolbar/md-toolbar'),
+    PLATFORM.moduleName('./md-card/md-card')
+  ]);
+}
--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ Bluebird.config({ warnings: { wForgottenReturn: false } });
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .feature(PLATFORM.moduleName('resources/index'));
+    .feature(PLATFORM.moduleName('components/index'));
 
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
